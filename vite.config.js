3be4e60845758643c450f7042bd21cca635b53ba
thedonmon <22967202+thedonmon@users.noreply.github.com>
2023-04-23 20:26:33 -0400

    update fucking vite

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,50 @@
 // vite.config.js
 import { sveltekit } from '@sveltejs/kit/vite';
 import { ssp } from "sveltekit-search-params/plugin";
-import { NgmiPolyfill } from "vite-plugin-ngmi-polyfill";
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
+import nodePolyfills from 'rollup-plugin-node-polyfills';
+
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [ssp(), sveltekit(), NgmiPolyfill()],
+	plugins: [ssp(), sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	},
-	optimizeDeps: {
-		include: ['@project-serum/anchor', '@solana/web3.js', 'buffer'],
-		// ... use the same implementation from the SvelteKit ui
-	},
-	define: {
-		'process.env': {}
+	resolve: {
+		alias: {
+		  stream: 'rollup-plugin-node-polyfills/polyfills/stream',
+		  events: 'rollup-plugin-node-polyfills/polyfills/events',
+		  assert: 'assert',
+		  crypto: 'crypto-browserify',
+		  util: 'util',
+		},
 	  },
+	  define: {
+		'process.env': process.env ?? {},
+	  },
+	  build: {
+		target: 'esnext',
+		rollupOptions: {
+		  plugins: [
+			nodePolyfills({ crypto: true }),
+		  ],
+		},
+	  },
+	  optimizeDeps: {
+		esbuildOptions: {
+		  plugins: [
+			NodeGlobalsPolyfillPlugin({ buffer: true }),
+		  ],
+		}
+	  },
+	// optimizeDeps: {
+	// 	include: ['@project-serum/anchor', '@solana/web3.js', 'buffer'],
+	// 	// ... use the same implementation from the SvelteKit ui
+	// },
+	// define: {
+	// 	'process.env': {}
+	//   },
 	server: {
 		fs: {
 			// https://vitejs.dev/config/server-options.html#server-fs-allow
